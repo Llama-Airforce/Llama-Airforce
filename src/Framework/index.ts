4bf6e32f847656d07d15c46799c4a98062b739fa
Alunara <>
2023-01-06 16:44:15 +0100

    Shorter framework barrel file

--- src/Framework/index.ts
+++ src/Framework/index.ts
@@ -1,61 +1,29 @@
-import AsyncValue from "@/Framework/AsyncValue.vue";
-import Button from "@/Framework/Button.vue";
-import ButtonToggle from "@/Framework/ButtonToggle.vue";
-import Card from "@/Framework/Card.vue";
-import CardGraph from "@/Framework/CardGraph.vue";
-import Code from "@/Framework/Code.vue";
-import Collapsible from "@/Framework/Collapsible.vue";
-import DataTable from "@/Framework/DataTable.vue";
-import DataTableRow from "@/Framework/DataTableRow.vue";
-import Header from "@/Framework/Header.vue";
-import HeaderDesktop from "@/Framework/HeaderDesktop.vue";
-import HeaderMobile from "@/Framework/HeaderMobile.vue";
-import InputNumber from "@/Framework/InputNumber.vue";
-import InputText from "@/Framework/InputText.vue";
-import KPI from "@/Framework/KPI.vue";
-import MenuDesktop from "@/Framework/MenuDesktop.vue";
-import MenuItem from "@/Framework/MenuItem.vue";
-import MenuMobile from "@/Framework/MenuMobile.vue";
-import Modal from "@/Framework/Modal.vue";
-import ModalYesNo from "@/Framework/ModalYesNo.vue";
-import Pagination from "@/Framework/Pagination.vue";
-import Select from "@/Framework/Select.vue";
-import SelectLanguage from "@/Framework/SelectLanguage.vue";
-import Slider from "@/Framework/Slider.vue";
-import { SortOrder } from "@/Framework/SortOrder";
-import Spinner from "@/Framework/Spinner.vue";
-import TabItem from "@/Framework/TabItem.vue";
-import TabView from "@/Framework/TabView.vue";
-import Tooltip from "@/Framework/ToolTip.vue";
-
-export {
-  AsyncValue,
-  Button,
-  ButtonToggle,
-  Card,
-  CardGraph,
-  Code,
-  Collapsible,
-  DataTable,
-  DataTableRow,
-  Header,
-  HeaderDesktop,
-  HeaderMobile,
-  InputNumber,
-  InputText,
-  KPI,
-  MenuDesktop,
-  MenuItem,
-  MenuMobile,
-  Modal,
-  ModalYesNo,
-  Pagination,
-  Select,
-  SelectLanguage,
-  Slider,
-  SortOrder,
-  Spinner,
-  TabItem,
-  TabView,
-  Tooltip,
-};
+export { default as AsyncValue } from "@/Framework/AsyncValue.vue";
+export { default as Button } from "@/Framework/Button.vue";
+export { default as ButtonToggle } from "@/Framework/ButtonToggle.vue";
+export { default as Card } from "@/Framework/Card.vue";
+export { default as CardGraph } from "@/Framework/CardGraph.vue";
+export { default as Code } from "@/Framework/Code.vue";
+export { default as Collapsible } from "@/Framework/Collapsible.vue";
+export { default as DataTable } from "@/Framework/DataTable.vue";
+export { default as DataTableRow } from "@/Framework/DataTableRow.vue";
+export { default as Header } from "@/Framework/Header.vue";
+export { default as HeaderDesktop } from "@/Framework/HeaderDesktop.vue";
+export { default as HeaderMobile } from "@/Framework/HeaderMobile.vue";
+export { default as InputNumber } from "@/Framework/InputNumber.vue";
+export { default as InputText } from "@/Framework/InputText.vue";
+export { default as KPI } from "@/Framework/KPI.vue";
+export { default as MenuDesktop } from "@/Framework/MenuDesktop.vue";
+export { default as MenuItem } from "@/Framework/MenuItem.vue";
+export { default as MenuMobile } from "@/Framework/MenuMobile.vue";
+export { default as Modal } from "@/Framework/Modal.vue";
+export { default as ModalYesNo } from "@/Framework/ModalYesNo.vue";
+export { default as Pagination } from "@/Framework/Pagination.vue";
+export { default as Select } from "@/Framework/Select.vue";
+export { default as SelectLanguage } from "@/Framework/SelectLanguage.vue";
+export { default as Slider } from "@/Framework/Slider.vue";
+export { SortOrder } from "@/Framework/SortOrder";
+export { default as Spinner } from "@/Framework/Spinner.vue";
+export { default as TabItem } from "@/Framework/TabItem.vue";
+export { default as TabView } from "@/Framework/TabView.vue";
+export { default as Tooltip } from "@/Framework/ToolTip.vue";
