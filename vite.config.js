42886a7457bdb12d1a4e97adea434bbb9dabc85c
Alunara <>
2023-02-15 00:41:15 +0100

    Update vite 4

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import path from "path";
 import process from "process";
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueI18n from "@intlify/vite-plugin-vue-i18n";
+import VueI18nPlugin  from "@intlify/unplugin-vue-i18n/vite";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [vue({ reactivityTransform: true }), vueI18n({
+    plugins: [vue({ reactivityTransform: true }), VueI18nPlugin({
       include: path.resolve(__dirname, './src/locales/**')
     })],
     server: {
