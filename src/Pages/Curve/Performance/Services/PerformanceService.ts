1b135d29e3476dfe07e60430e4858c74dec4fa95
benber86 <benoitb@tutanota.de>
2023-02-19 20:10:05 +1100

    Remove logging

--- src/Pages/Curve/Performance/Services/PerformanceService.ts
+++ src/Pages/Curve/Performance/Services/PerformanceService.ts
@@ -9,7 +9,6 @@ export class PoolPerformanceResponse {
 export default class PerformanceService extends ServiceBase {
   public get(pool: string, startDate: number, endDate: number, lpAmount: number): Promise<PoolPerformanceResponse> {
     const url = `https://api-py.llama.airforce/curve/v1/pools/mainnet/returns/${pool}?start_date=${startDate}&end_date=${endDate}&lp_tokens=${lpAmount}`;
-    console.log(url);
     return this.fetch(url, PoolPerformanceResponse);
   }
 }
