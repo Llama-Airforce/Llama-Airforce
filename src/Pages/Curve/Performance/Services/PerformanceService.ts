19d62e2c065c4bdbb09312774aea5f276dcbf443
benber86 <benoitb@tutanota.de>
2023-02-19 11:52:54 +1100

    Update API endpoint

--- src/Pages/Curve/Performance/Services/PerformanceService.ts
+++ src/Pages/Curve/Performance/Services/PerformanceService.ts
@@ -8,7 +8,7 @@ export class PoolPerformanceResponse {
 
 export default class PerformanceService extends ServiceBase {
   public get(pool: string, startDate: number, endDate: number, lpAmount: number): Promise<PoolPerformanceResponse> {
-    const url = `http://localhost:5001/curve/v1/pools/mainnet/returns/${pool}?start_date=${startDate}&end_date=${endDate}&lp_tokens=${lpAmount}`;
+    const url = `https://api-py.llama.airforce/curve/v1/pools/mainnet/returns/${pool}?start_date=${startDate}&end_date=${endDate}&lp_tokens=${lpAmount}`;
     console.log(url);
     return this.fetch(url, PoolPerformanceResponse);
   }
