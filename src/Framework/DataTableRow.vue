41b284b44e11187db31b7bc7da32aabdd33324b5
Alunara <>
2023-02-24 00:52:58 +0100

    Show pointer cursor when datatable row is expandable

--- src/Framework/DataTableRow.vue
+++ src/Framework/DataTableRow.vue
@@ -5,7 +5,7 @@
   >
     <div
       class="row-data item"
-      :class="[columns, { active: selected }]"
+      :class="[columns, { active: selected, expandable }]"
       @click="onClick"
     >
       <slot name="row"></slot>
@@ -84,6 +84,10 @@ const onClick = (): void => {
   flex-direction: column;
 
   > .row-data {
+    &.expandable:hover {
+      cursor: pointer;
+    }
+
     > .expander {
       > i {
         transition: transform 125ms cubic-bezier(0.65, 0.05, 0.36, 1);
