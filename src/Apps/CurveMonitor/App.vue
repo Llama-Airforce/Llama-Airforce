7ecba6363a7284157d1f7fe69ff3f807a760870f
Alunara <>
2023-07-23 14:41:00 +0200

    Fix breadcrumb margin on bigger screens

--- src/Apps/CurveMonitor/App.vue
+++ src/Apps/CurveMonitor/App.vue
@@ -181,10 +181,12 @@ p {
     overflow-y: auto;
 
     > .breadcrumb {
-      max-width: calc(1920px - 23.25rem);
+      max-width: calc(1920px - 18.125rem);
       margin: auto;
       margin-top: var(--page-margin);
       margin-bottom: calc(var(--page-margin) * -1 + var(--dashboard-gap));
+      padding-left: var(--page-margin);
+      padding-right: var(--page-margin);
 
       @media only screen and (max-width: 1280px) {
         padding: 0 1rem;
