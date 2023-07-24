84ccc100b47bc4e81c55cd69b76c627fc1eb6824
Alunara <>
2023-07-24 02:06:40 +0200

    Less words

--- src/Apps/CurveMonitor/Pages/Pool/Components/MEV/Sandwiches.vue
+++ src/Apps/CurveMonitor/Pages/Pool/Components/MEV/Sandwiches.vue
@@ -70,9 +70,11 @@
             }}
             {{ props.item.user_losses_details[0].unit }}
           </span>
-          (that's
-          {{ roundPhil(-props.item.user_losses_details[0].lossInPercentage) }}%
-          slippage, or ${{ roundPhil(props.item.lossInUsd) }})
+          <span>
+            {{
+              roundPhil(-props.item.user_losses_details[0].lossInPercentage)
+            }}% slippage, or ${{ roundPhil(props.item.lossInUsd) }}
+          </span>
         </div>
       </div>
 
