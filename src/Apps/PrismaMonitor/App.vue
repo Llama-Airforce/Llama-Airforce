51b6e55634f5473dc8908c2ef6543c397b6d9655
Alunara <>
2023-10-05 19:05:37 +0200

    Remove breadcrumb from mkUSD pages

--- src/Apps/PrismaMonitor/App.vue
+++ src/Apps/PrismaMonitor/App.vue
@@ -50,7 +50,7 @@ const router = useRouter();
 watch(
   () => route.fullPath,
   (newRoute) => {
-    if (newRoute.includes("pool")) {
+    if (newRoute.includes("pool") || newRoute.includes("mkusd")) {
       storeBreadcrumb.show = false;
     }
   }
