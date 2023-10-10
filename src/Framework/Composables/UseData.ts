a59123ad84d7ddd691afb9652f808215a7384809
Alunara <>
2023-10-11 01:18:57 +0200

    Add docs

--- src/Framework/Composables/UseData.ts
+++ src/Framework/Composables/UseData.ts
@@ -1,6 +1,11 @@
 import { ref, type Ref } from "vue";
 import { tryNotify } from "@/Util";
 
+/**
+ * Vue composable to async load data and handle errors through notifications.
+ * @param f The async factory function that retrieves data.
+ * @param init The default / init value of the data before loading is complete.
+ */
 export function useData<T>(f: () => Promise<T>, init: T) {
   const loading = ref(false);
   const data = ref<T>(init) as Ref<T>;
--- src/Util/NotificationHelper.ts
+++ src/Util/NotificationHelper.ts
@@ -1,5 +1,9 @@
 import { notify } from "@kyvg/vue3-notification";
 
+/**
+ * Calls a function that might throw, and shows caught exceptions as notifications.
+ * @param f The function to call that might throw.
+ */
 export async function tryNotify<T>(
   f: () => Promise<T>
 ): Promise<T | undefined> {
