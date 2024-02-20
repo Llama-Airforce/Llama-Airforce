492b8be54e040037f0f97aa400461954125d08e0
Alunara <>
2024-02-21 00:32:02 +0100

    Remove CVX from metric tooltip

--- src/Apps/LlamaAirforce/Pages/Bribes/Overview/Components/Summary.vue
+++ src/Apps/LlamaAirforce/Pages/Bribes/Overview/Components/Summary.vue
@@ -89,7 +89,7 @@ const rewardsPerDollarBribeTooltip = computed((): string => {
 
   switch (protocol.value) {
     case "cvx-crv":
-      tokens = "CRV + CVX";
+      tokens = "CRV";
       break;
     case "aura-bal":
       tokens = "BAL + AURA";
