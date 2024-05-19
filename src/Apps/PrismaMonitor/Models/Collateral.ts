a013d8dc271c31df8d011f2feb91877c222619e6
Alunara <>
2024-05-19 20:06:26 +0200

    Move lst and lsd token icons to assets folder

--- src/Apps/PrismaMonitor/Models/Collateral.ts
+++ src/Apps/PrismaMonitor/Models/Collateral.ts
@@ -4,31 +4,25 @@ export const collaterals = [...collateralsLsd, ...collateralsLrt] as const;
 
 export type Collateral = (typeof collaterals)[number];
 
+import cbETH from "@/Assets/Icons/Tokens/cbeth.webp";
+import wstETH from "@/Assets/Icons/Tokens/steth.webp";
+import rETH from "@/Assets/Icons/Tokens/reth.webp";
+import sfrxETH from "@/Assets/Icons/Tokens/frxeth.webp";
+
+import weETH from "@/Assets/Icons/Tokens/weeth.webp";
+import ezETH from "@/Assets/Icons/Tokens/ezeth.svg";
+import rsETH from "@/Assets/Icons/Tokens/rseth.svg";
+
 export function icon(collateral: Collateral) {
-  const getLogo = (collateral: Collateral) => {
-    switch (collateral) {
-      // LSD
-      case "cbETH":
-        return "cbeth.webp";
-      case "wstETH":
-        return "steth.webp";
-      case "rETH":
-        return "reth.webp";
-      case "sfrxETH":
-        return "frxeth.webp";
-
-      // LRT
-      case "weETH":
-        return "weeth.webp";
-      case "ezETH":
-        return "ezeth.svg";
-      case "rsETH":
-        return "rseth.svg";
-
-      default:
-        return "";
-    }
+  const mapping: Record<Collateral, string> = {
+    cbETH,
+    wstETH,
+    rETH,
+    sfrxETH,
+    weETH,
+    ezETH,
+    rsETH,
   };
 
-  return `icons/tokens/${getLogo(collateral)}`;
+  return mapping[collateral];
 }
