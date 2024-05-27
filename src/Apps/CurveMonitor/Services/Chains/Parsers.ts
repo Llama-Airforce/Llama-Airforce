b11b9fee83f5b012cce20ffc8fd00b9a1ac7a6dc
Alunara <>
2024-05-27 23:17:17 +0200

    Fix copy paste error in chain volume

--- src/Apps/CurveMonitor/Services/Chains/Parsers.ts
+++ src/Apps/CurveMonitor/Services/Chains/Parsers.ts
@@ -9,7 +9,7 @@ export const parseChainInfo = (
     total: {
       tvl: x.total.total_tvl,
       tradingVolume24h: x.total.trading_volume_24h,
-      tradingFee24h: x.total.trading_volume_24h,
+      tradingFee24h: x.total.trading_fee_24h,
       liquidityVolume24h: x.total.liquidity_volume_24h,
       liquidityFee24h: x.total.liquidity_fee_24h,
     },
