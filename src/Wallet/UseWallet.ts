cd10e229b37ff920cf2709a7c6eff6c13a5cb32b
Alunara <>
2024-06-19 01:46:06 +0200

    Remove ethers from useWallet

--- src/Wallet/UseWallet.ts
+++ src/Wallet/UseWallet.ts
@@ -1,7 +1,3 @@
-import {
-  type JsonRpcProvider,
-  type JsonRpcSigner,
-} from "@ethersproject/providers";
 import { type Network } from "@/Wallet/Network";
 import {
   useClient,
@@ -12,7 +8,6 @@ import {
   useConnectorClient,
 } from "@wagmi/vue";
 import { base, mainnet } from "viem/chains";
-import { clientToProvider, clientToSigner } from "@/Wallet/Wagmi";
 
 // eslint-disable-next-line max-lines-per-function
 export function useWallet() {
@@ -26,7 +21,7 @@ export function useWallet() {
   const { address, isConnected, chainId, connector } = useAccount();
 
   const addressOnProvider = computed(() =>
-    provider.value && address.value ? address.value : undefined
+    connectorClient.data.value && address.value ? address.value : undefined
   );
 
   const network = computed((): Network | undefined => {
@@ -43,97 +38,9 @@ export function useWallet() {
   // Connectors and providers
   const connectorClient = useConnectorClient();
   const providerCowSwap = computedAsync(async () => {
-    const prov = (await connector.value?.getProvider()) as JsonRpcProvider;
+    const prov = await connector.value?.getProvider();
     return prov;
   });
-  const provider = computed(() =>
-    connectorClient.data.value
-      ? clientToProvider(connectorClient.data.value)
-      : undefined
-  );
-  const signer = computed(() =>
-    connectorClient.data.value
-      ? clientToSigner(connectorClient.data.value)
-      : undefined
-  );
-
-  const withProvider = <T extends [...unknown[]]>(
-    func: (
-      provider: JsonRpcProvider,
-      address: string,
-      ...args: T
-    ) => Promise<void>,
-    ifNot?: (...args: T) => void
-  ): ((...args: T) => Promise<void>) => {
-    return (...args: T) => {
-      if (!provider.value || !address.value) {
-        return new Promise<void>((resolve) => {
-          if (ifNot) {
-            ifNot(...args);
-          }
-
-          resolve(); // resolve with no value
-        });
-      }
-
-      return func(provider.value, address.value, ...args);
-    };
-  };
-
-  const withProviderReturn = <T extends [...unknown[]], R>(
-    func: (
-      provider: JsonRpcProvider,
-      address: string,
-      ...args: T
-    ) => Promise<R>,
-    ifNot: (...args: T) => R
-  ): ((...args: T) => Promise<R>) => {
-    return (...args: T) => {
-      if (!provider.value || !address.value) {
-        return new Promise<R>((resolve) => {
-          const x = ifNot(...args);
-          resolve(x);
-        });
-      }
-
-      return func(provider.value, address.value, ...args);
-    };
-  };
-
-  const withSigner = <T extends [...unknown[]]>(
-    func: (signer: JsonRpcSigner, address: string, ...args: T) => Promise<void>,
-    ifNot?: (...args: T) => void
-  ): ((...args: T) => Promise<void>) => {
-    return (...args: T) => {
-      if (!signer.value || !address.value) {
-        return new Promise<void>((resolve) => {
-          if (ifNot) {
-            ifNot(...args);
-          }
-
-          resolve(); // resolve with no value
-        });
-      }
-
-      return func(signer.value, address.value, ...args);
-    };
-  };
-
-  const withSignerReturn = <T extends [...unknown[]], R>(
-    func: (provider: JsonRpcSigner, address: string, ...args: T) => Promise<R>,
-    ifNot: (...args: unknown[]) => R
-  ): ((...args: T) => Promise<R>) => {
-    return async (...args: T) => {
-      if (!signer.value || !address.value) {
-        return new Promise<R>((resolve) => {
-          const x = ifNot(...args);
-          resolve(x);
-        });
-      }
-
-      return func(signer.value, address.value, ...args);
-    };
-  };
 
   return {
     client,
@@ -143,12 +50,6 @@ export function useWallet() {
     connectors,
     network,
     address: addressOnProvider,
-    provider,
     providerCowSwap,
-    signer,
-    withProvider,
-    withProviderReturn,
-    withSigner,
-    withSignerReturn,
   };
 }
--- src/Wallet/Wagmi.ts
+++ src/Wallet/Wagmi.ts
@@ -1,8 +1,6 @@
 import { createConfig, http } from "@wagmi/core";
 import { mainnet } from "@wagmi/core/chains";
 import { injected, coinbaseWallet, safe } from "@wagmi/connectors";
-import type { Account, Chain, Client, Transport } from "viem";
-import { providers } from "ethers";
 
 export const config = createConfig({
   chains: [mainnet],
@@ -25,26 +23,3 @@ export const config = createConfig({
     [mainnet.id]: http(undefined, { batch: { wait: 100 } }),
   },
 });
-
-// Functions for ethers migration.
-export function clientToProvider(client: Client<Transport, Chain>) {
-  const { chain, transport } = client;
-  const network = {
-    chainId: chain.id,
-    name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
-  };
-  return new providers.Web3Provider(transport, network);
-}
-
-export function clientToSigner(client: Client<Transport, Chain, Account>) {
-  const { account, chain, transport } = client;
-  const network = {
-    chainId: chain.id,
-    name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
-  };
-  const provider = new providers.Web3Provider(transport, network);
-  const signer = provider.getSigner(account.address);
-  return signer;
-}
