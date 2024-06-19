a46e95e4616242e1b37b2223497cc90b85c88a77
Alunara <>
2024-06-19 02:18:59 +0200

    Add back walletconnect

--- src/Wallet/Wagmi.ts
+++ src/Wallet/Wagmi.ts
@@ -1,20 +1,25 @@
 import { createConfig, http } from "@wagmi/core";
 import { mainnet } from "@wagmi/core/chains";
-import { injected, coinbaseWallet, safe } from "@wagmi/connectors";
+import {
+  injected,
+  walletConnect,
+  coinbaseWallet,
+  safe,
+} from "@wagmi/connectors";
 
 export const config = createConfig({
   chains: [mainnet],
   connectors: [
     injected(),
-    /*
-     * walletConnect({
-     *   projectId: "7a61bbd683f613a5308ca86fd4cb14f7",
-     *   showQrModal: true,
-     *   qrModalOptions: {
-     *     themeMode: "dark",
-     *   },
-     * }),
-     */
+
+    walletConnect({
+      projectId: "7a61bbd683f613a5308ca86fd4cb14f7",
+      showQrModal: true,
+      qrModalOptions: {
+        themeMode: "dark",
+      },
+    }),
+
     coinbaseWallet(),
     safe(),
   ],
