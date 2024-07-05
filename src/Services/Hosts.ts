133265db120b594c31d2649f32a6cbe50634855f
Alunara <>
2024-07-05 16:08:11 +0200

    Use LAF api for CMfor now

--- src/Services/Hosts.ts
+++ src/Services/Hosts.ts
@@ -3,7 +3,7 @@ import { type App } from "@/Framework/Apps";
 export const hosts: Record<App, string | undefined> = {
   laf: "https://api.llama.airforce",
   cb: undefined,
-  cm: undefined,
+  cm: "https://api.llama.airforce",
   pm: undefined,
   "pm-lrt": undefined,
 };
