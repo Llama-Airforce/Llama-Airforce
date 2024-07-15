d3d9b7c64fae91dd362bed2a88811de5169ed223
Alunara <>
2024-07-15 16:30:02 +0200

    No longer cache individual airdrop claims, airdrop itself is cached

--- src/Apps/LlamaAirforce/Server/routes/airdrop/[airdropId]/[address].get.ts
+++ src/Apps/LlamaAirforce/Server/routes/airdrop/[airdropId]/[address].get.ts
@@ -4,41 +4,38 @@ import { getAirdropClaims } from "@LAF/Server/util/getAirdropClaims";
 
 export type Result = Awaited<ReturnType<typeof handler>>;
 
-const handler = defineCachedEventHandler(
-  async (event) => {
-    const { airdropId, address } = getRouterParams(event);
+const handler = defineEventHandler(async (event) => {
+  const { airdropId, address } = getRouterParams(event);
 
-    if (!isAirdropId(airdropId)) {
-      throw createError({
-        statusCode: 400,
-        message: "Invalid airdrop id",
-      });
-    }
+  if (!isAirdropId(airdropId)) {
+    throw createError({
+      statusCode: 400,
+      message: "Invalid airdrop id",
+    });
+  }
 
-    if (!isAddress(address)) {
-      throw createError({
-        statusCode: 400,
-        message: "Invalid address parameter",
-      });
-    }
+  if (!isAddress(address)) {
+    throw createError({
+      statusCode: 400,
+      message: "Invalid address parameter",
+    });
+  }
 
-    const claims = await getAirdropClaims(airdropId);
-    const claim = claims.claims[address];
+  const claims = await getAirdropClaims(airdropId);
+  const claim = claims.claims[address];
 
-    if (!claim) {
-      throw createError({
-        statusCode: 404,
-        message: `Claim for '${address}' not found`,
-      });
-    }
+  if (!claim) {
+    throw createError({
+      statusCode: 404,
+      message: `Claim for '${address}' not found`,
+    });
+  }
 
-    return {
-      index: claim.index,
-      amount: claim.amount,
-      proof: claim.proof.map((x) => x as Address) as readonly Address[],
-    };
-  },
-  { maxAge: 60 }
-);
+  return {
+    index: claim.index,
+    amount: claim.amount,
+    proof: claim.proof.map((x) => x as Address) as readonly Address[],
+  };
+});
 
 export default handler;
