a53ca2dc11f808b47bb4e323901aaa01ab2f66f1
Alunara <>
2024-08-06 00:03:38 +0200

    Touchup transfers table

--- src/Apps/CurveMonitor/Pages/DefiMonitor/Tables/TableTransfers.vue
+++ src/Apps/CurveMonitor/Pages/DefiMonitor/Tables/TableTransfers.vue
@@ -3,12 +3,14 @@
     class="datatable-transfers"
     :rows="rowsPage"
     :columns="[
-      '',
-      'Token',
-      'Amount',
+      'Hash',
+      'Block',
+      'Gas',
       'From',
       'To',
-      { label: 'Time', align: 'end' },
+      'Amount',
+      'Token',
+      { label: 'Age', align: 'end' },
     ]"
     :loading
   >
@@ -36,22 +38,34 @@
     </template>
 
     <template #row="{ item }: { item: CleanedTransfer }">
-      <TokenIcon
-        chain="ethereum"
-        :address="item.coinAddress"
-      ></TokenIcon>
+      <div class="hash">
+        <a
+          class="font-mono"
+          :href="`https://etherscan.io/tx/${item.txHash}`"
+          target="_blank"
+        >
+          {{ addressLeft(item.txHash, 10) }}
+        </a>
+
+        <Button
+          icon="fas fa-link"
+          @click="clipboard(item.txHash)"
+        ></Button>
+      </div>
 
       <div>
         <a
           class="font-mono"
+          :href="`https://etherscan.io/block/${item.blockNumber}`"
           target="_blank"
-          :href="`https://etherscan.io/address/${item.coinAddress}`"
         >
-          {{ item.coinSymbol }}
+          {{ item.blockNumber }}
         </a>
       </div>
 
-      <div>{{ round(item.parsedAmount) }}</div>
+      <div>
+        {{ item.gasInGwei }}
+      </div>
 
       <div>
         <a
@@ -59,7 +73,7 @@
           :href="`https://etherscan.io/address/${item.transferFrom}`"
           target="_blank"
         >
-          {{ addressShort(item.transferFrom) }}
+          {{ addressShort(item.transferFrom, 10) }}
         </a>
       </div>
 
@@ -69,7 +83,23 @@
           :href="`https://etherscan.io/addr ess/${item.transferTo}`"
           target="_blank"
         >
-          {{ addressShort(item.transferTo) }}
+          {{ addressShort(item.transferTo, 10) }}
+        </a>
+      </div>
+
+      <div>{{ round(item.parsedAmount) }}</div>
+
+      <div class="token">
+        <TokenIcon
+          chain="ethereum"
+          :address="item.coinAddress"
+        ></TokenIcon>
+
+        <a
+          target="_blank"
+          :href="`https://etherscan.io/address/${item.coinAddress}`"
+        >
+          {{ item.coinSymbol }}
         </a>
       </div>
 
@@ -88,7 +118,7 @@
 
 <script setup lang="ts">
 import { chain } from "lodash";
-import { addressShort } from "@/Wallet";
+import { addressShort, addressLeft } from "@/Wallet";
 import { type CleanedTransfer } from "@CM/Services/Monitor/Transfers";
 import { useQueryTransfers } from "@CM/Services/Monitor/Transfers/Queries";
 
@@ -107,7 +137,15 @@ const transfers = computed(() =>
       const includesTerm = (x: string): boolean =>
         terms.some((term) => x.toLocaleLowerCase().includes(term));
 
-      return includesTerm(row.transferFrom) || includesTerm(row.transferTo);
+      // Min amount parsing.
+      const parsedAmount = terms.length === 1 ? parseFloat(terms[0]) : NaN;
+      const minAmount = !isNaN(parsedAmount) ? parsedAmount : 0;
+
+      return (
+        includesTerm(row.transferFrom) ||
+        includesTerm(row.transferTo) ||
+        (minAmount > 0 && row.parsedAmount >= minAmount)
+      );
     })
     .orderBy([(x) => x.blockUnixtime, (x) => x.positionInBlock], "desc")
     .value()
@@ -118,6 +156,10 @@ const { page, rowsPage, onPage } = usePagination(transfers, rowsPerPage);
 
 const round = (x: number) =>
   x < 1 ? x.toFixed(4) : x > 1000 ? x.toFixed(0) : x.toFixed(2);
+
+const clipboard = async (addr: string) => {
+  await navigator.clipboard.writeText(addr);
+};
 </script>
 
 <style lang="scss" scoped>
@@ -125,8 +167,37 @@ const round = (x: number) =>
 
 .datatable-transfers {
   --columns-header: 1fr 2fr;
-  --columns-data: 26px 6rem minmax(5rem, 1fr) minmax(5rem, 1fr)
-    minmax(5rem, 1fr) minmax(5rem, 1fr);
+  --columns-data: minmax(6rem, 1fr) minmax(5rem, 1fr) 5rem minmax(5rem, 1fr)
+    minmax(5rem, 1fr) minmax(5rem, 1fr) calc(26px + 8rem) minmax(5rem, 1fr);
+
+  :deep(.row-data) {
+    grid-column-gap: 2rem;
+  }
+
+  button {
+    background: transparent;
+
+    &:hover,
+    &:active {
+      background: transparent;
+    }
+  }
+
+  .hash {
+    display: flex;
+    align-items: center;
+    gap: 1ch;
+  }
+
+  .token {
+    display: flex;
+    align-items: center;
+    gap: 1ch;
+
+    img {
+      height: 26px;
+    }
+  }
 }
 </style>
 
