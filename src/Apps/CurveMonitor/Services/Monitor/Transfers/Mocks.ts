75cb0e25e279480f6c7489329484ab174b219fa0
Alunara <>
2024-08-06 21:57:27 +0200

    Add possibility of mock transfer observable

---
+++ src/Apps/CurveMonitor/Services/Monitor/Transfers/Mocks.ts
@@ -0,0 +1,32 @@
+import { Observable } from "rxjs";
+import { type USDCBlockSummary } from "./Models";
+
+const mockUSDCBlockSummary: USDCBlockSummary = {
+  blockNumber: 17000000,
+  blockUnixtime: 1686000000,
+  transfers: [
+    [
+      {
+        transferFrom: "0x1234567890123456789012345678901234567890",
+        transferTo: "0x0987654321098765432109876543210987654321",
+        parsedAmount: 1000000,
+        coinAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        coinSymbol: "USDC",
+        txHash:
+          "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890",
+        blockNumber: 17000000,
+        blockUnixtime: 1686000000,
+        positionInBlock: 0,
+        gasInGwei: 20,
+        contractCaller: "0x2468101214161820222426283032343638404242",
+        calledContract: "0x1357911131517192123252729313335373941434",
+      },
+    ],
+  ],
+};
+
+export const mockUSDCBlockSummaryObservable = () =>
+  new Observable<USDCBlockSummary>((subscriber) => {
+    subscriber.next(mockUSDCBlockSummary);
+    subscriber.complete();
+  });
--- src/Apps/CurveMonitor/Services/Monitor/Transfers/TransfersService.ts
+++ src/Apps/CurveMonitor/Services/Monitor/Transfers/TransfersService.ts
@@ -8,11 +8,16 @@ type SocketObservable<T extends keyof ServerToClientEvents> = ReturnType<
   typeof createObservable<ServerToClientEvents, T>
 >;
 
+type Observables = {
+  transers$?: SocketObservable<"NewTransfersUSDC">;
+};
+
 export default class TransfersService {
   public readonly transfers$: SocketObservable<"NewTransfersUSDC">;
 
-  constructor(private socket: SocketMonitorDefi) {
-    this.transfers$ = createObservable(socket, "NewTransfersUSDC");
+  constructor(private socket: SocketMonitorDefi, observables?: Observables) {
+    this.transfers$ =
+      observables?.transers$ ?? createObservable(socket, "NewTransfersUSDC");
   }
 
   subTransfers() {
