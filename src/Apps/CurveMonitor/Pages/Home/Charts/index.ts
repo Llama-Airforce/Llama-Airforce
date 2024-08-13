80134fb51a8fd21f6aa7f4fc74190e0041c680e9
Alunara <>
2024-08-13 17:08:35 +0200

    Almost empty out the existing landing page

--- src/Apps/CurveMonitor/Pages/Home/Charts/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ChartChainType } from "./ChartChainType.vue";
-export { default as ChartLiquidityTop } from "./ChartLiquidityTop.vue";
-export { default as ChartRanking } from "./ChartRanking.vue";
--- src/Apps/CurveMonitor/Pages/Home/Home.page.vue
+++ src/Apps/CurveMonitor/Pages/Home/Home.page.vue
@@ -1,41 +1,11 @@
 <template>
   <div class="home">
-    <div class="controls-container">
-      <InputText
-        v-model="pool"
-        class="search"
-        placeholder="Search for a pool: enter a space here"
-        :search="true"
-      >
-      </InputText>
-
-      <!-- <Controls></Controls> -->
-    </div>
-
-    <div class="data-1">
-      <KPIs></KPIs>
-      <ChartChainType></ChartChainType>
-      <ChartLiquidityTop></ChartLiquidityTop>
-    </div>
-
-    <div class="data-2">
-      <TableTrades></TableTrades>
-      <ChartRanking></ChartRanking>
-    </div>
+    <KPIs></KPIs>
   </div>
 </template>
 
 <script setup lang="ts">
 import KPIs from "@CM/Pages/Home/Components/KPIs.vue";
-import {
-  ChartChainType,
-  ChartLiquidityTop,
-  ChartRanking,
-} from "@CM/Pages/Home/Charts";
-import TableTrades from "@CM/Pages/Home/Tables/TableTrades.vue";
-
-// Refs
-const pool = ref("");
 </script>
 
 <style lang="scss" scoped>
@@ -45,44 +15,5 @@ const pool = ref("");
 
 .home {
   max-width: calc(1920px - 18.125rem);
-}
-
-.controls-container {
-  display: none;
-
-  box-shadow: var(--container-box-shadow);
-  border-radius: var(--border-radius);
-  overflow: hidden;
-
-  :deep(.search) {
-    input {
-      background: var(--c-lvl1);
-      border-bottom-left-radius: 0px;
-      border-bottom-right-radius: 0px;
-      border: 0;
-    }
-  }
-}
-
-.data-1 {
-  display: grid;
-  grid-template-columns: 10rem 1fr 1fr;
-  gap: var(--dashboard-gap);
-
-  @media only screen and (max-width: 1280px) {
-    display: flex;
-    flex-direction: column;
-  }
-}
-
-.data-2 {
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  gap: var(--dashboard-gap);
-
-  @media only screen and (max-width: 1280px) {
-    display: flex;
-    flex-direction: column;
-  }
 }
 </style>
