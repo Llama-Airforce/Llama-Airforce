804ac087efe39070751d21e4969d7ce60f8e6291
Alunara <>
2024-08-15 18:04:41 +0200

    Remove MonitorLegacy

--- src/Apps/CurveMonitor/Models/TimeRange.ts
+++
@@ -1,3 +0,0 @@
-export const timeRanges = ["day", "week", "month"] as const;
-
-export type TimeRange = typeof timeRanges[number];
--- src/Apps/CurveMonitor/Models/index.ts
+++
@@ -1,4 +0,0 @@
-export type { PoolId, Pool } from "./Pool";
-export type { Coin } from "./Coin";
-export type { Pair } from "./Pair";
-export type { TimeRange } from "./TimeRange";
