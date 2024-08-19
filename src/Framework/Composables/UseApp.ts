903f1ebf74161c787669e7ef8ce36723455d3e37
Alunara <>
2024-08-19 18:02:47 +0200

    Fix lint error

--- src/Framework/Composables/UseApp.ts
+++ src/Framework/Composables/UseApp.ts
@@ -1,12 +1,16 @@
 import { type App, apps } from "@/Framework/Apps";
 
 export function useApp() {
-  const app = import.meta.env.VITE_APP as App;
+  const app = import.meta.env.VITE_APP as App | undefined;
+
+  if (!app) {
+    const error = "Missing app for useApp()";
+    notify({ text: error, type: "error" });
+    throw new Error(error);
+  }
 
   if (!apps.includes(app)) {
-    const error = !app
-      ? "Missing app for useApp()"
-      : `Unknown app for useApp(): ${app}`;
+    const error = `Unknown app for useApp(): ${app}`;
 
     notify({ text: error, type: "error" });
     throw new Error(error);
