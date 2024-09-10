ae3fa04a7878ebb0d40b9c948f6b5c7f2e3f95c4
Alunara <>
2024-09-10 22:18:06 +0200

    Use CSS instead of SCSS for utils

--- src/Apps/setup.ts
+++ src/Apps/setup.ts
@@ -5,7 +5,7 @@ import Notifications, { notify } from "@kyvg/vue3-notification";
 import { hashFn } from "@wagmi/core/query";
 import { type CreateConnectorFn, WagmiPlugin } from "@wagmi/vue";
 import { createConfig as createConfigWagmi } from "@/Wallet/Wagmi";
-import "@/Styles/Util.scss";
+import "@/Styles/Util.css";
 
 /** Options for configuring the setup function */
 type Options = {
