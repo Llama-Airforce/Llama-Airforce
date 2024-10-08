55fbde356ff6e437881aef58bddf012a051f7a02
Alunara <>
2024-10-09 00:02:45 +0200

    Cleanup tsconfig.json

--- src/Framework/TabItem.vue
+++ src/Framework/TabItem.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { header = "", disabled = false } = defineProps<{
+defineProps<{
   header?: string;
   disabled?: boolean;
 }>();
