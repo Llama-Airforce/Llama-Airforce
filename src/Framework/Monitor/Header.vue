b35324553c721c57226ebfdc9bd54c2b7c7865bd
Alunara <>
2024-10-31 20:46:00 +0100

    Fix header background for light themes

--- src/Framework/Monitor/Header.vue
+++ src/Framework/Monitor/Header.vue
@@ -10,7 +10,7 @@ header {
   gap: 1rem;
   padding: 1rem var(--page-margin);
 
-  background-color: var(--c-lvl0);
+  background-color: var(--header-background, var(--c-lvl0));
   border-bottom: 1px solid var(--c-lvl1);
 
   @media only screen and (max-width: 1280px) {
