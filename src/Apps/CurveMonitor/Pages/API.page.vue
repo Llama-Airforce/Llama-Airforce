9c740cd5da469aeed914a93d38d2827cb764a72d
Alunara <>
2024-12-06 01:58:16 +0100

    Lazily import Swagger for CurveMonitor

--- src/Apps/CurveMonitor/Pages/API.page.vue
+++ src/Apps/CurveMonitor/Pages/API.page.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const Swagger = defineAsyncComponent(
+  () => import("@/Framework/Components/Swagger.vue")
+);
+</script>
 
 <template>
   <Swagger url="https://prices.curve.fi/feeds-docs/openapi.json" />
