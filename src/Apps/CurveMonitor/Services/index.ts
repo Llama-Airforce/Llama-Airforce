ce9e2e5fd7072f0c99d7007d8b71c9078a0cf1f6
Alunara <>
2025-01-23 18:41:03 +0100

    Handle milliseconds when parsing time

--- src/Apps/CurveMonitor/Services/index.ts
+++ src/Apps/CurveMonitor/Services/index.ts
@@ -18,7 +18,8 @@ export function toUTC(timestamp: string | number): number {
 
   const [date, time] = timestamp.split("T");
   const [year, month, day] = date.split("-").map(Number);
-  const [hour, minute, second] = time.split(":").map(Number);
+  const [timeWithoutZ] = time.split("."); // Handle milliseconds by splitting on dot
+  const [hour, minute, second] = timeWithoutZ.split(":").map(Number);
 
   return Date.UTC(year, month - 1, day, hour, minute, second) / 1000;
 }
