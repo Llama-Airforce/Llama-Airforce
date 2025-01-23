f33431da2f860b4b4b36f7a9f9967254d2812ff2
Alunara <>
2025-01-24 00:29:14 +0100

    Avoid using bigNumToNumber

--- src/Apps/CurveMonitor/Services/proposal/parsers.ts
+++ src/Apps/CurveMonitor/Services/proposal/parsers.ts
@@ -1,4 +1,3 @@
-import { bigNumToNumber } from "@/Utils/Number";
 import { toUTC } from "..";
 import type * as Responses from "./responses";
 import type * as Models from "./models";
@@ -20,13 +19,13 @@ export const parseProposal = (
   const block = x.snapshot_block;
   const start = x.start_date;
   const end = start + 604800;
-  const quorum = bigNumToNumber(BigInt(x.min_accept_quorum), 18n);
-  const support = bigNumToNumber(BigInt(x.support_required), 18n);
+  const quorum = Number(BigInt(x.min_accept_quorum)) / 10 ** 18;
+  const support = Number(BigInt(x.support_required)) / 10 ** 18;
   const voteCount = x.vote_count;
-  const votesFor = bigNumToNumber(BigInt(x.votes_for), 18n);
-  const votesAgainst = bigNumToNumber(BigInt(x.votes_against), 18n);
+  const votesFor = Number(BigInt(x.votes_for)) / 10 ** 18;
+  const votesAgainst = Number(BigInt(x.votes_against)) / 10 ** 18;
   const executed = x.executed;
-  const totalSupply = bigNumToNumber(BigInt(x.total_supply), 18n);
+  const totalSupply = Number(BigInt(x.total_supply)) / 10 ** 18;
   const txCreation = x.transaction_hash;
 
   return {
@@ -60,7 +59,7 @@ export const parseProposalDetails = (
   const votes = x.votes.map((vote) => ({
     voter: vote.voter.toLocaleLowerCase(),
     supports: vote.supports,
-    votingPower: bigNumToNumber(BigInt(vote.voting_power), 18n),
+    votingPower: Number(BigInt(vote.voting_power)) / 10 ** 18,
   }));
 
   return {
