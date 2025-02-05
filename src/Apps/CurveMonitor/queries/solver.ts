a48417bd3765d035b7cc690be00d4fe76ce008d5
Alunara <>
2025-02-05 14:49:02 +0100

    Fix incorrect API host url for CoW solver

--- src/Apps/CurveMonitor/queries/solver.ts
+++ src/Apps/CurveMonitor/queries/solver.ts
@@ -1,9 +1,12 @@
 import type { Address } from "@CM/Services";
 import * as Api from "@CM/Services/solver";
 
+const API_URL = "https://api.cow.fi";
+
 export function useQuerySolverCompetition(tx: Ref<Address>) {
   return useQuery({
     queryKey: ["solver-competition", tx] as const,
-    queryFn: ({ queryKey: [, tx] }) => Api.getCompetition(tx),
+    queryFn: ({ queryKey: [, tx] }) =>
+      Api.getCompetition(tx, { host: API_URL }),
   });
 }
