ecd7121e3d6f00d772668565b179c1090c6bc1a3
Alunara <>
2025-03-26 21:11:42 +0100

    Add API page

---
+++ src/Apps/HippoArmy/Pages/API.page.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const Swagger = defineAsyncComponent(
+  () => import("@/Framework/Components/Swagger.vue")
+);
+</script>
+
+<template>
+  <Swagger url="https://api.hippo.army/feeds-docs/openapi.json" />
+</template>
