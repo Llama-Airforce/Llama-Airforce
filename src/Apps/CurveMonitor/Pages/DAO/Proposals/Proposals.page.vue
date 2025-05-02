fa911fbd802aaf7c0a68d657d3af97b2fa19bb88
Alunara <>
2025-05-02 23:53:54 +0200

    Update packages

--- src/Apps/CurveMonitor/Pages/DAO/Proposals/Proposals.page.vue
+++ src/Apps/CurveMonitor/Pages/DAO/Proposals/Proposals.page.vue
@@ -15,7 +15,7 @@ const pageDebounced = refDebounced(page, 200);
 const proposalSearch = ref("");
 const proposalSearchDebounced = refDebounced(proposalSearch, 300);
 
-const proposalType = ref<ProposalType>("all");
+const proposalType = ref<ProposalType | "all">("all");
 
 const proposalStatus = computed((): ProposalStatus => {
   switch (tabActive.value) {
